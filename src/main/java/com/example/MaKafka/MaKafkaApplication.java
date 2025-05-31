bc6993bc870cd4ccbecdb478ec05acf89f52da31
Maksim Kryzhanovskiy <kryzhanovskiy@secretgroup.ru>
2025-05-31 19:24:47 +0300

    init

---
+++ src/main/java/com/example/MaKafka/MaKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.MaKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaKafkaApplication.class, args);
+	}
+
+}
